f874f859421297abf8ccee8c5e80dda46714e15f
namandoshi8 <workspace.naman@gmail.com>
2025-01-22 16:17:57 +0530

    use authentication and logut created

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model");
 const userServices = require("../services/user.services");
 const { body, validationResult } = require("express-validator");
+const blacklistTokenModel = require("../models/blacklistToken.model");
 
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -44,6 +45,7 @@ module.exports.loginUser = async (req, res, next) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
     const token = await user.generateAuthToken();
+    res.cookie("token", token);
     user.token = token;
     res.status(200).json({ token, user });
   } catch (error) {
@@ -53,4 +55,15 @@ module.exports.loginUser = async (req, res, next) => {
 
 module.exports.getUserProfile = async (req, res, next) => {
   res.status(200).json(req.user);
+};
+
+module.exports.logoutUser = async (req, res, next) => {
+  try {
+    res.clearCookie("token");
+    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+    await blacklistTokenModel.create({ token });
+    res.status(200).json({ message: "Logout successful" });
+  } catch (error) {
+    next(error);
+  }
 };
--- BackEnd/middleware/auth.middleware.js
+++ BackEnd/middleware/auth.middleware.js
@@ -1,10 +1,15 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const blacklistTokenModel = require("../models/blacklistToken.model");
 
 module.exports.authUser = async (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1] || req.cookies.token;
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
   if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  const blacklistedToken = await blacklistTokenModel.findOne({ token });
+  if (blacklistedToken) {
     return res.status(401).send({ message: "Unauthorized" });
   }
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
