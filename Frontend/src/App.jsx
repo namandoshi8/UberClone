7c023bd6ea87de75727094ca11e7c12815fb73ed
namandoshi8 <workspace.naman@gmail.com>
2025-01-23 00:39:10 +0530

    connected user end routes to backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,30 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 import UserLogin from "./pages/UserLogin";
 import UserSignUp from "./pages/UserSignUp";
 import CaptainLogin from "./pages/CaptainLogin";
 import CaptainSignUp from "./pages/CaptainSignUp";
+import HomePage from "./pages/HomePage";
+import UserProtectWrapper from "./pages/UserProtectWrapper";
 
 function App() {
   return (
     <div>
       <Routes>
         {/* <Route path="*" element={<Home />} /> */}
-        <Route path="/" element={<Home />} index />
+        <Route path="/" element={<Landing />} index />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/signup" element={<UserSignUp />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
         <Route path="/captain-signup" element={<CaptainSignUp />} />
+        <Route
+          path="/home"
+          element={
+            <UserProtectWrapper>
+              <HomePage />
+            </UserProtectWrapper>
+          }
+        />
       </Routes>
     </div>
   );
